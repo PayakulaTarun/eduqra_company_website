e9f05f5b7f85613363f78a1440c212dcf264178d
EDUQRA <eduqratech@gmail.com>
2026-04-10 07:18:24 +0530

    feat: secure welcome email trigger with environment variables and live credentials

--- react_website/functions/index.js
+++ react_website/functions/index.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'eduqratech@gmail.com',
-    pass: 'YOUR_APP_PASSWORD' 
+    pass: process.env.GMAIL_PASS 
   }
 });
 
